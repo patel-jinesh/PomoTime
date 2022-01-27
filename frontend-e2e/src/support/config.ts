23edf04167d6fa0792bae912f90a4be13aa33d8c
Jinesh Patel <programmer.jinesh@gmail.com>
2022-01-27 10:58:35 -0600

    Playwright + Cucumber + Typescript

---
+++ frontend-e2e/src/support/config.ts
@@ -0,0 +1,15 @@
+import { LaunchOptions } from 'playwright';
+const browserOptions: LaunchOptions = {
+  slowMo: 1000,
+  headless: false,
+  args: ['--use-fake-ui-for-media-stream', '--use-fake-device-for-media-stream'],
+  firefoxUserPrefs: {
+    'media.navigator.streams.fake': true,
+    'media.navigator.permission.disabled': true,
+  },
+};
+
+export const config = {
+  browserOptions,
+  BASE_URL: 'https://playwright.dev',
+};
