ce554eab92379f9ad212178dd5cd1d76a948d2cc
Jinesh Patel <programmer.jinesh@gmail.com>
2022-01-29 01:18:58 -0600

    Remove default app

--- frontend/src/hooks.ts
+++ frontend/src/hooks.ts
@@ -1,5 +1,5 @@
 import cookie from 'cookie';
-import { v4 as uuid } from '@lukeed/uuid';
+import { v4 as uuid } from 'uuid';
 import type { Handle } from '@sveltejs/kit';
 
 export const handle: Handle = async ({ event, resolve }) => {
